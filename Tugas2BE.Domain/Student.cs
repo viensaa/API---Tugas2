d4252ec995854cf04283db6f2320157eff01fd19
rizal <rizal@DESKTOP-23RBQ8Q>
2022-08-05 18:57:34 +0700

    Add project files.

---
+++ Tugas2BE.Domain/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas2BE.Domain
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+
+}
