25c252953e99b337ee99527a65b83e28ac87e3da
rizal <rizal@DESKTOP-23RBQ8Q>
2022-08-15 19:25:54 +0700

    membuat create pake dropdown

--- Tugas2FE/Controllers/EnrollmentController.cs
+++ Tugas2FE/Controllers/EnrollmentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Tugas2FE.Interfaces;
 using Tugas2FE.ViewModels;
 
@@ -7,15 +8,20 @@ namespace Tugas2FE.Controllers
     public class EnrollmentController : Controller
     {
         private readonly IEnrollment _enrollmentDAL;
+        private readonly ICourse _courseDAL;
+        private readonly IStudent _studentDAL;
 
-        public EnrollmentController(IEnrollment  enrollment)
+        public EnrollmentController(IEnrollment  enrollment,ICourse course,IStudent student)
         {
             _enrollmentDAL = enrollment;
+            _courseDAL = course;
+            _studentDAL = student;
         }
         //getdALL
         public async Task<IActionResult> Index()
         {
             ViewData["pesan"] = TempData["pesan"] ?? TempData["pesan"];
+
             IEnumerable<EnrollmentDetail> models;
             models = await _enrollmentDAL.GetAll();
             return View(models);
@@ -23,17 +29,21 @@ namespace Tugas2FE.Controllers
 
         //insert
         //insert data        
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
-
+            ViewBag.Course =new SelectList(await _courseDAL.GetAll(), "courseID", "title");
+            ViewBag.Student =new SelectList(await _studentDAL.GetAll(), "id", "lastName");
             return View();
         }
-
+        
         [HttpPost]
         public async Task<ActionResult> Create(Enrollment enrollment)
         {
+            
             try
             {
+                ViewBag.Course = new SelectList(await _courseDAL.GetAll(), "courseID", "title");
+                ViewBag.Student = new SelectList(await _studentDAL.GetAll(), "id", "lastName");
                 var result = await _enrollmentDAL.Insert(enrollment);
                 TempData["pesan"] = $"<div class='alert alert-success alert-dismissible fade show'><button type='button' class='btn-close' data-bs-dismiss='alert'></button> Berhasil Melakukan enrollment  Dengan Id {result.EnrollmentID}</div>";
                 return RedirectToAction("Index");
