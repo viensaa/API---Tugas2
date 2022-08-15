e9b414fa9c163ad0615dd78d0c8777a12fe7a229
rizal <rizal@DESKTOP-23RBQ8Q>
2022-08-15 09:07:56 +0700

    mengubah output enrollment gel all

---
+++ Tugas2BE/DTO/EnrollmentDetailDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Tugas2BE.DTO
+{
+    public class EnrollmentDetailDTO
+    {
+        public int EnrollmentID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
