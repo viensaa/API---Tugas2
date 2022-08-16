84f993c6e9c16997b3f675e2f9a0561e9acfab3a
rizal <rizal@DESKTOP-23RBQ8Q>
2022-08-16 20:18:13 +0700

    setting up account

---
+++ Tugas2FE/Interfaces/IAccount.cs
@@ -0,0 +1,11 @@
+﻿using Tugas2FE.ViewModels;
+
+namespace Tugas2FE.Interfaces
+{
+    public interface IAccount
+    {
+        Task<Account> Register(Account obj);
+        Task<SLogin> Authenticate(string username, string password);
+
+    }
+}
---
+++ Tugas2FE/Services/AccountServices.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using Tugas2FE.Interfaces;
+using Tugas2FE.ViewModels;
+
+namespace Tugas2FE.Services
+{
+    public class AccountServices : IAccount
+    {
+        public Task<SLogin> Authenticate(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Account> Register(Account obj)
+        {
+            Account account = new Account();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(obj),
+                    Encoding.UTF8, "application/json");
+                using (var response = await httpClient.PostAsync($"https://localhost:8001/api/User", content))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.Created)
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        account = JsonConvert.DeserializeObject<Account>(apiResponse);
+                    }
+                }
+            }
+            return account;
+        }
+    }
+}
